a8abef032c41b167130f91ba31836545919a8089
rjwaliczek <r.j.waliczek@gmail.com>
2026-04-08 07:41:41 +0200

    implement Game class with init, run render and clean methods

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,15 +1,53 @@
+#pragma once
 #include "Game.h"
 #include <stdio.h>
+#include <SDL2/SDL_video.h>
 
 Game::Game() = default;
 Game::~Game() = default;
 //SDL/windows initialization, 
 bool Game::init() {
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL init error %s\n", SDL_GetError());
+        SDL_Quit();
+        return false;
+    }
+    
+    window = SDL_CreateWindow(
+        "HELIUM3 v0.1",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+    if(!window) {
+        printf("Create window error %s\n", SDL_GetError());
+        SDL_Quit();
+        return false;
+    }
+    renderer = SDL_CreateRenderer (
+        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if(!renderer) {
+        printf("Renderer error. %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return false;
+    }
 
+    isRunning = true;
+    printf("HELIUM3 is running successfully.\n");
+    return true;
 }
 //Event handler
 void Game::handleEvents() {
-
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        if(event.type == SDL_QUIT) {
+            isRunning = false;
+        }
+    }
 }
 //Game logic placeholder 
 void Game::update() {
@@ -17,13 +55,29 @@ void Game::update() {
 }
 //screen cleaning and background drawing
 void Game::render() {
-
+    SDL_SetRenderDrawColor(renderer, 0, 75, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
 //game main loop
 void Game::run() {
+    while(isRunning) {
+        handleEvents();
+        update();
+        render();
+    }
 
 }
 //SDL resources cleaner
 void Game::clean() {
-
+    if(renderer) {
+        SDL_DestroyRenderer(renderer);
+        renderer = nullptr;
+    }
+    if (window) {
+        SDL_DestroyWindow(window);
+        window = nullptr;
+    }
+    SDL_Quit();
+    printf("Program HELIUM3 terminated successfully.\n");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <stdio.h>
+#include "Game.h"
 
 
 
-int main() {
-    std::cout << "hello";
+int main(int argc, char* argv[]) {
+    Game game;
+    game.init();
+    game.run();
+    game.clean();
+
     return 0;
 }
